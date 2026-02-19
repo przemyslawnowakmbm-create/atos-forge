1d3c2c7cc350b484993d8ea22c44d88fe7fee213
przemyslawnowakmbm-create <przemyslawnowakmbm-create@users.noreply.github.com>
2026-02-19 22:37:01 +0100

    test: change hello signature

--- test-update.js
+++ test-update.js
@@ -1,4 +1,3 @@
-export function hello(name) { return name; }
+export function hello(name, greeting) { return `${greeting} ${name}`; }
 export function greet(title, name) { return `${title} ${name}`; }
 export class Greeter { constructor(prefix) { this.prefix = prefix; } }
-// updated
