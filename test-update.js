c9d4ba45d3d4984a70128dc70bcbeab0d329f245
przemyslawnowakmbm-create <przemyslawnowakmbm-create@users.noreply.github.com>
2026-02-19 22:34:30 +0100

    test: add file for updater test

---
+++ test-update.js
@@ -0,0 +1,3 @@
+export function hello(name) { return name; }
+export function greet(title, name) { return `${title} ${name}`; }
+export class Greeter { constructor(prefix) { this.prefix = prefix; } }
