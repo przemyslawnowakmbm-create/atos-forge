4bb5b4b34cb9589dd4411dd893a46f58e9e64d16
przemyslawnowakmbm-create <przemyslawnowakmbm-create@users.noreply.github.com>
2026-02-19 22:37:32 +0100

    test: delete test file

--- test-update.js
+++
@@ -1,3 +0,0 @@
-export function hello(name, greeting) { return `${greeting} ${name}`; }
-export function greet(title, name) { return `${title} ${name}`; }
-export class Greeter { constructor(prefix) { this.prefix = prefix; } }
